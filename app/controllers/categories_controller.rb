5fce927011f3677fadd215c9a31fe1e5225cceaa
alexrcenteno <alex.centeno1991@gmail.com>
2023-02-08 14:08:23 -0600

    added index new show create edit and delete actions for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,11 @@ class CategoriesController < ApplicationController
     render :index
   end
 
+  def show
+    @category = Category.find_by(id: params[:id])
+    render :show
+  end
+
   def new
     @category = Category.new
     render :new
@@ -14,5 +19,24 @@ class CategoriesController < ApplicationController
       name: params[:category][:name],
     )
     redirect_to "/categories"
+  end
+
+  def edit
+    @category = Category.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.update(
+      name: params[:category][:name],
+    )
+    redirect_to "/categories"
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+    redirect_to "/categories", status: :see_other
   end
 end
