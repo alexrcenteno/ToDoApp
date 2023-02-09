7ab693aa15e977cb8dd4970e95a957569633e199
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-02-09 12:17:30 -0600

    created routes, controller for category_todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :todos
-
+  resources :category_todos
   resources :categories
   resources :users
-
 
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
