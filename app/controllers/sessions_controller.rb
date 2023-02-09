a3bf2a936ae0d0acace2998ef86e8fbe143e275f
alexrcenteno <alex.centeno1991@gmail.com>
2023-02-09 11:33:51 -0600

    adds a user signup and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    render template: "sessions/new"
+  end
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      jwt = JWT.encode(
-        {
-          user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i, # the expiration time
-        },
-        Rails.application.credentials.fetch(:secret_key_base), # the secret key
-        "HS256" # the encryption algorithm
-      )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      session[:user_id] = user.id
+      redirect_to "/"
     else
-      render json: {}, status: :unauthorized
+      redirect_to "/login"
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
   end
 end
