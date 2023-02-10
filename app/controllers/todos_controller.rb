5c0addcfab3289f3d6a24e3208a8c52d6a40d49d
dallinforsyth <dallfor192@gmail.com>
2023-02-10 12:28:25 -0600

    added todos by categories

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,28 +29,54 @@ class TodosController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+    # TODO: This is new stuff!!!
+    if params[:category]
+      print("THIS IS RUNNING and the category name is " + params[:category])
+      cat = Category.find_by(name: params[:category])
+      print("CAT ID IS #{cat.id}")
+      cat_todo = CategoryTodo.new(todo_id: @todo.id, category_id: cat.id)
+      if cat_todo.save
+        print("THAT SHIT SAVED")
+      else
+        print("MY GUY " + cat_todo.errors.full_messages.to_s)
+      end
+    else
+      print("IT AIN'T RUNNING")
+    end
   end
 
   def edit
     @todo = Todo.find_by(id: params[:id])
-    render :edit
+    if @todo.user_id == current_user.id
+      render :edit
+    end
   end
 
   def update
     @todo = Todo.find_by(id: params[:id])
-    @todo.update(
-      user_id: params[:todo][:user_id],
-      title: params[:todo][:title],
-      description: params[:todo][:description],
-      deadline: params[:todo][:deadline],
-      completed: params[:todo][:completed],
-    )
-    redirect_to "/todos"
+    if @todo.user_id == current_user.id
+      if @todo.update(
+        title: params[:todo][:title],
+        description: params[:todo][:description],
+        deadline: params[:todo][:deadline],
+        completed: params[:todo][:completed],
+      )
+        redirect_to "/todos"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      redirect_to "/todos", status: :see_other
+    end
   end
 
   def destroy
     @todo = Todo.find_by(id: params[:id])
-    @todo.destroy
-    redirect_to "/todos", status: :see_other
+    if @todo.user_id == current_user.id
+      @todo.destroy
+      redirect_to "/todos", status: :see_other
+    else
+      redirect_to "/cats", status: :see_other
+    end
   end
 end
--- app/models/category_todo.rb
+++ app/models/category_todo.rb
@@ -1,5 +1,4 @@
 class CategoryTodo < ApplicationRecord
-  
   belongs_to :category
   belongs_to :todo
 end
